4c43f213db2df5a0ddefbdfe27b8e35a6052ba21
Michał Urban <michalurban23@gmail.com>
2018-01-17 14:06:31 +0100

    Add constructors to DynamicIntArray

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -3,4 +3,19 @@ package com.codecool.dynamicArrayDojo;
 // put your code here!
 public class DynamicIntArray {
 
+    private int size;
+
+    public DynamicIntArray() {
+
+        this.size = 0;
+    }
+
+    public DynamicIntArray(int size) {
+
+        this.size = size;
+    }
+
+    public void add(int value) {}
+    public void insert(int index, int value) {}
+    public void remove(int value) {}
 }
