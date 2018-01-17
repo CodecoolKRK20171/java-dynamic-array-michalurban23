01bc43bc42c5079229d3fffc5adff70eeefb5f6a
Michał Urban <michalurban23@gmail.com>
2018-01-17 15:10:20 +0100

    Implement all methods to pass tests

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -1,21 +1,82 @@
 package com.codecool.dynamicArrayDojo;
 
-// put your code here!
 public class DynamicIntArray {
 
-    private int size;
+    private int[] array;
 
     public DynamicIntArray() {
 
-        this.size = 0;
+        this.array = new int[0];
     }
 
     public DynamicIntArray(int size) {
 
-        this.size = size;
+        this.array = new int[size];
     }
 
-    public void add(int value) {}
-    public void insert(int index, int value) {}
-    public void remove(int value) {}
+    public void add(int value) {
+
+        int[] newArray = new int[array.length + 1];
+
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+        }
+        newArray[array.length] = value;
+
+        this.array = newArray;
+    }
+
+    public void insert(int index, int value) {
+
+        if (index >= array.length) {
+            add(value);
+        } else {
+            insertValue(index, value);
+        }
+    }
+
+    public void remove(int index) {
+
+        int[] newArray = new int[array.length - 1];
+
+        copySubArray(newArray, index);
+
+        for (int i = index; i < newArray.length; i++) {
+            newArray[i] = array[i+1];
+        }
+
+        this.array = newArray;
+    }
+
+    private void insertValue(int index, int value) {
+
+        int[] newArray = new int[array.length + 1];
+
+        copySubArray(newArray, index);
+        newArray[index] = value;
+
+        for (int i = index; i < array.length; i++) {
+            newArray[i+1] = array[i];
+        }
+        this.array = newArray;
+    }
+
+    private void copySubArray(int[] newArray, int index) {
+
+        for (int i = 0; i < index; i++) {
+            newArray[i] = array[i];
+        }
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int value: array) {
+            sb.append(" " + value);
+        }
+        return sb.toString();
+    }
+
 }
